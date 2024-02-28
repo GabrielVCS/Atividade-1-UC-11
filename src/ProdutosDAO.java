7ae86f4ca292bc580e4d544264cac25d5de6b6bd
Gabriel Viana <gabvc2011@outlook.com>
2024-02-27 21:54:21 -0300

    Método cadastrar adicionado + ajustes em 'conectaDAO'

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -7,36 +7,45 @@
  *
  * @author Adm
  */
-
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
     Connection conn;
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public int cadastrarProduto(ProdutosDTO produto) {
+
+        conn = new conectaDAO().getConexao();
+
+        int status1;
+        String sql = "insert into produtos (nome, valor, status) values(?,?,?)";
+
+        try {
+            prep = this.conn.prepareStatement(sql);
+            
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+
+            status1 = prep.executeUpdate();
+            return status1; //retornar 1
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar0" + ex.getMessage());
+            return ex.getErrorCode();
+        }
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
